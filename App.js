fe49fa6d08e2d0713a63df390c524da8331ed410
soniyaAlpha <soniya@alphadxb.com>
2026-04-17 11:49:33 +0530

    hello world program in vanilla JS and then in react via cdn links

---
+++ App.js
@@ -0,0 +1,31 @@
+/*
+<div id="parent">
+    <div id="child1">
+        <h1>h1 tag</h1>
+        <h2>h2 tag</h2/>
+    </div>
+    <div id="child2">
+        <h1>h1 tag</h1>
+        <h2>h2 tag</h2/>
+    </div>
+</div>
+
+*/
+
+const h1 = React.createElement("div",{"id":"parent"},[
+    React.createElement("div",{"id":"child1"},
+        [
+            React.createElement("h1",{"id":"heading1"},"I'm heading 1 "),
+            React.createElement("h2",{"id":"heading2"},"I'm heading 2")
+        ]
+    ),
+    React.createElement("div",{"id":"child2"},
+        [
+            React.createElement("h1",{"id":"heading3"},"I'm heading 3 "),
+            React.createElement("h2",{"id":"heading4"},"I'm heading 4")
+        ]
+    )
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(h1);
